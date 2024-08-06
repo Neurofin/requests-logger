06c6bfdb907f2eb6eebc2a1c6cfd1a385c54fc60
Neeraj-Neurofin <neeraj@neurofin.ai>
2024-08-07 00:59:07 +0530

    Created logUtils folder

--- middleware/requestLogger.middleware.go
+++ middleware/requestLogger.middleware.go
@@ -2,28 +2,16 @@ package middleware
 
 import (
 	"bytes"
-	"encoding/json"
 	"io"
-	"log"
 	"net/http"
-	"os"
 	"time"
 
+	"github.com/Neurofin/requests-logger/logUtils"
 	logTypeEnum "github.com/Neurofin/requests-logger/store/enum"
 	loggerTypes "github.com/Neurofin/requests-logger/store/types"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
-
-type CustomResponseWriter struct {
-	http.ResponseWriter
-	body *bytes.Buffer
-}
-
-func (w *CustomResponseWriter) Write(b []byte) (int, error) {
-	w.body.Write(b)
-	return w.ResponseWriter.Write(b)
-}
 
 func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
@@ -48,7 +36,7 @@ func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 
 		// Create a custom response writer to capture response body
 		resBody := new(bytes.Buffer)
-		crw := &CustomResponseWriter{ResponseWriter: res.Writer, body: resBody}
+		crw := &loggerTypes.CustomResponseWriter{ResponseWriter: res.Writer, Body: resBody}
 		res.Writer = crw
 
 		// Proceed with the request
@@ -58,7 +46,7 @@ func LoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 
 		// Log the request and response details
 		logRequest(req, requestBody.Bytes(), start, traceId)
-		logResponse(res, crw.body.Bytes(), crw.Header(), start, end, traceId)
+		logResponse(res, crw.Body.Bytes(), crw.Header(), start, end, traceId)
 
 		return err
 	}
@@ -86,7 +74,7 @@ func logRequest(req *http.Request, requestBody []byte, start time.Time, traceId 
 		Timestamp: time.Now(),
 	}
 
-	postLog(logInput)
+	logUtils.PostLog(logInput)
 }
 
 func logResponse(res *echo.Response, responseBody []byte, responseHeaders http.Header, start, end time.Time, traceId string) {
@@ -109,33 +97,33 @@ func logResponse(res *echo.Response, responseBody []byte, responseHeaders http.H
 		Timestamp: time.Now(),
 	}
 
-	postLog(logInput)
+	logUtils.PostLog(logInput)
 }
 
-func postLog(logInput loggerTypes.PostLogInput) {
-	logInputJSON, err := json.Marshal(logInput)
-	if err != nil {
-		log.Printf("Error marshaling log data: %v", err)
-		return
-	}
+// func postLog(logInput loggerTypes.PostLogInput) {
+// 	logInputJSON, err := json.Marshal(logInput)
+// 	if err != nil {
+// 		log.Printf("Error marshaling log data: %v", err)
+// 		return
+// 	}
 
-	logServiceURL := os.Getenv("LOG_SERVICE_URL")
-	if logServiceURL == "" {
-		log.Printf("LOG_SERVICE_URL is not set")
-		return
-	}
+// 	logServiceURL := os.Getenv("LOG_SERVICE_URL")
+// 	if logServiceURL == "" {
+// 		log.Printf("LOG_SERVICE_URL is not set")
+// 		return
+// 	}
 
-	go func() {
-		resp, err := http.Post(logServiceURL, "application/json", bytes.NewBuffer(logInputJSON))
-		if err != nil {
-			log.Printf("Error posting log data: %v", err)
-			return
-		}
-		defer resp.Body.Close()
+// 	go func() {
+// 		resp, err := http.Post(logServiceURL, "application/json", bytes.NewBuffer(logInputJSON))
+// 		if err != nil {
+// 			log.Printf("Error posting log data: %v", err)
+// 			return
+// 		}
+// 		defer resp.Body.Close()
 
-		if resp.StatusCode != http.StatusCreated {
-			body, _ := io.ReadAll(resp.Body)
-			log.Printf("Unexpected response from log service: %s", body)
-		}
-	}()
-}
+// 		if resp.StatusCode != http.StatusCreated {
+// 			body, _ := io.ReadAll(resp.Body)
+// 			log.Printf("Unexpected response from log service: %s", body)
+// 		}
+// 	}()
+// }
