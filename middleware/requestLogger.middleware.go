fb4dc2768bdd07f0239d1ce4c86208e1b138acc8
Neeraj-Neurofin <neeraj@neurofin.ai>
2024-09-24 23:38:21 +0530

    minor fixes

--- middleware/requestLogger.middleware.go
+++ middleware/requestLogger.middleware.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"time"
 	"github.com/Neurofin/requests-logger/logUtils"
-	loggerTypes "github.com/Neurofin/requests-logger/store/types"
+	"github.com/Neurofin/requests-logger/store/types"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
@@ -17,13 +17,21 @@ func LoggingMiddleware(service string) echo.MiddlewareFunc {
 			req := c.Request()
 			res := c.Response()
 
-			user := c.Get("user").(loggerTypes.UserDetails)
-
 			traceId := req.Header.Get("traceId")
 			if traceId == "" {
 				traceId = uuid.New().String()
 				req.Header.Set("traceId", traceId)
 				c.Set("traceId", traceId)
+			}
+
+			var userDetails loggerTypes.UserDetails
+			if user, ok := c.Get("user").(map[string]interface{}); ok {
+				if firstName, exists := user["firstName"].(string); exists {
+					userDetails.FirstName = firstName
+				}
+				if email, exists := user["email"].(string); exists {
+					userDetails.Email = email
+				}
 			}
 
 			// Capture request headers
@@ -44,7 +52,7 @@ func LoggingMiddleware(service string) echo.MiddlewareFunc {
 			end := time.Now()
 
 			// Log the request and response details asynchronously
-			go logUtils.LogRequestResponse(req, requestBody.Bytes(), res, crw.Body.Bytes(), crw.Header(), start, end, traceId, service, user)
+			go logUtils.LogRequestResponse(req, requestBody.Bytes(), res, crw.Body.Bytes(), crw.Header(), start, end, traceId, service, userDetails)
 
 			return err
 		}
--- store/types/userDetails.type.go
+++ store/types/userDetails.type.go
@@ -1,26 +1,6 @@
 package loggerTypes
 
-import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type UserDetails struct {
-	Id              primitive.ObjectID              `json:"id,omitempty"`
-	Org             primitive.ObjectID              `json:"org"`
-	FirstName       string                          `json:"firstName"`
-	LastName        string                          `json:"lastName"`
-	Email           string                          `json:"email"`
-	Phone           string                          `json:"phone,omitempty"`
-	Type            string              			`json:"type"`
-	Verified        bool                            `json:"verified"`
-	Permissions     []string 						`json:"permissions"`
-	CognitoUsername string                          `json:"cognitoUsername,omitempty"`
-	Timestamps
-}
-
-type Timestamps struct {
-	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+	FirstName  string   `json:"firstName"`
+	Email      string   `json:"email"`
 }
